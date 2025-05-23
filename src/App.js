ce98307e11710518353733416a6569b21c5a6dc1
Medeirozleo <medeirozleo@gmail.com>
2025-05-22 22:04:02 -0300

    criacao das pastas e arquivos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './Login/Login.js';
+import "./Login/Login.css";
 
 function App() {
   return (
-    <div className="Teste_git">
-    <h1>Hello Mundo</h1>
+    <div>
+    <Login />
     </div>
   );
 }
---
+++ src/Login/Login.js
@@ -0,0 +1,9 @@
+function Login(){
+    return(
+        <div className="teste">
+            <p>Hello World!</p>
+        </div>
+    )
+}
+
+export default Login;
