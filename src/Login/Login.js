7c8922d553771349530a33e0675367153eb160bb
Medeirozleo <medeirozleo@gmail.com>
2025-05-25 17:12:11 -0300

    realizado a criação do estilo e botões

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,7 +1,25 @@
 function Login(){
     return(
-        <div className="teste">
-            <p>Hello World!</p>
+        <div className="background">
+            <div className="box">
+                <h1 className="logo">
+                    <span className="eco">eco</span>
+                    <span className="farm">farm</span>
+                </h1>
+                <div className="botoes">
+                    <div className="div_ajuste">
+                        <div className="botao_estilo">
+                            <p>novo jogo</p>
+                        </div>
+                        <div className="botao_estilo">
+                            <p>continuar</p>
+                        </div>
+                        <div className="botao_estilo">
+                            <p>sair</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
