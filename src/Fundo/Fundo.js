00af7d252b4bf9a97b09cc2b3d6bcdf0e5762a6e
gallego06 <lucas.marcio475@gmail.com>
2025-06-03 22:34:28 -0300

    Grid do fundo de tela

--- src/Fundo/Fundo.js
+++ src/Fundo/Fundo.js
@@ -3,12 +3,19 @@ import './Fundo.css';
 function Fundo() {
   return (
     <> 
-      <div className="ContainerCaixa">
-        <div className="Caixa1"> <p>ola</p> </div>
-        <div className="Caixa1"> <p>oi</p> </div>
+    
+      <div className="ContainerGeral">
+          <div className='ContainerCaixa1'>1</div>
+          <div className='ContainerCaixa2'>2</div>
+          <div className='ContainerCaixa3'>3
+            <div class="borda-inferior"></div>
+            </div>  
+          <div className='ContainerCaixa4'>4
+            <div class="borda-inferior"></div>
+            </div> 
       </div>
-      <div className="terra">
-      </div>
+
+      <div className="terra"></div>
     </>
   );
 }
